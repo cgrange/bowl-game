f975f82a32aaa0170d4f580e5c78f9641cdfd891
Colton Grange <coltonbgrange@gmail.com>
2020-04-20 21:32:21 -0500

    move state up from landing page and add hidden class to top element if gameStarted

--- client/src/LandingPage/LandingPage.js
+++ client/src/LandingPage/LandingPage.js
@@ -5,8 +5,12 @@ import './LandingPage.scss';
 const axios = require('axios');
 const rce = React.createElement;
 
-function LandingPage() {
-    const [timeLimit, setTimeLimit] = useState(45);
+function LandingPage(props) {
+    const timeLimit = props.state.timeLimit;
+    const setTimeLimit = (value) => {
+        props.state.timeLimit = value;
+        props.setState(props.state);
+    };
 
     function begin() {
         axios
@@ -16,13 +20,15 @@ function LandingPage() {
             .then(res => {
                 console.log(`statusCode: ${res.statusCode}`)
                 console.log(res)
+                props.state.gameStarted = true;
+                props.setState(props.state);
             })
             .catch(error => {
                 console.error(error)
             });
     }
 
-    return rce('div', {className: 'LandingPage'},
+    return rce('div', {className: {'LandingPage ' + props.state.gameStarted ?  'hidden' : ''}},
         rce('h5', {className: 'header'},
             'To start a new game enter the time limit for each round, then click begin'
         ),
