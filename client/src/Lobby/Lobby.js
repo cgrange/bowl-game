2d05ffc977cb1ef0e12eb7bee40aacd7f800a260
Colton Grange <coltonbgrange@gmail.com>
2020-04-24 15:24:46 -0500

    style the lobby and add arena button

--- client/src/Lobby/Lobby.js
+++ client/src/Lobby/Lobby.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import './Lobby.scss';
 
 const rce = React.createElement;
 
 function Lobby(props) {
     const lobby = JSON.parse(JSON.stringify(props.state));
 
+    function enterArena() {
+        alert('entering arena!');
+    }
+
     return rce('div', {className: 'Lobby'},
-        rce('h3', {className: 'team-score'}, 'Team 1: ' + lobby.team1Score),
-        rce('h5', {className: 'prompts-left'}, 'Prompts Left: ' + lobby.promptsLeft),
-        rce('h3', {className: 'team-score'}, 'Team 2: ' + lobby.team2Score),
-        rce('h4', {className: 'turn-tracker'}, lobby.team1sTurn ? 'Team 1\'s Turn' : 'Team 2\'s Turn')
+        rce('div', {className: 'scoreboard'},
+            rce('h1', {className: 'team-score'}, 'Team 1: ' + lobby.team1Score),
+            rce('h4', {className: 'prompts-left'}, 'Prompts Left: ' + lobby.promptsLeft),
+            rce('h1', {className: 'team-score'}, 'Team 2: ' + lobby.team2Score),
+        ),
+        rce('h4', {className: 'turn-tracker'}, lobby.team1sTurn ? 'Team 1\'s Turn' : 'Team 2\'s Turn'),
+        rce('button', {className: 'arena-button', onClick: enterArena}, 'ENTER ARENA!')
     );
 }
 
